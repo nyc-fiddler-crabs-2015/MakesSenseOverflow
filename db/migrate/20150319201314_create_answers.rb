24a05a530e825efdd778eb2530dff382628ed02a
Christopher Selden & Justin Gaba & Dongchul Lee <pair+chrisseldo.gabalicious.i-love-ruby@devbootcamp.com>
2015-03-19 16:30:24 -0400

    Fixed Answers Migration

--- db/migrate/20150319201314_create_answers.rb
+++ db/migrate/20150319201314_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :content, null: false
-      t.integer :vote_count, null: false, default: 0
+      t.integer :vote_count, default: 0
       t.references :question
       t.references :user
 

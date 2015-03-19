c3976545fdc67821a19be5f120e0dced8b489470
Christopher Selden & Justin Gaba & Dongchul Lee <pair+chrisseldo.gabalicious.i-love-ruby@devbootcamp.com>
2015-03-19 16:29:57 -0400

    Completed Questions Migration

--- db/migrate/20150319201426_create_questions.rb
+++ db/migrate/20150319201426_create_questions.rb
@@ -1,6 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :vote_count, default: 0
+      t.references :user, null: false
+
+      t.timestamps null: false
     end
   end
 end

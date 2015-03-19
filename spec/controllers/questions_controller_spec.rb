7fa3259dce2d0a3efaaa38af6a35e727d480f368
Christopher Selden & Justin Gaba & Dongchul Lee <pair+chrisseldo.gabalicious.i-love-ruby@devbootcamp.com>
2015-03-19 18:00:45 -0400

    catered test to factory girl

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,14 +4,9 @@ require 'pp'
 describe QuestionsController do
 
   describe 'create new question' do
-    context "POST /questions" do
-      before :each do
-        question :create, {question: { title: "Hello", description: "This is a great question!", user_id: 1 } }
-      end
-
-      it 'should have a new entry in the database' do
-        expect(Question.all.count).to eq(1)
-      end
+    let(:user) { create(:question) }
+    it 'should have a new entry in the database' do
+      expect(Question.all.count).to eq(1)
     end
   end
 end

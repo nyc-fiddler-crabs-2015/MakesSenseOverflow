de661ecc07a5a4661a6ecfa34c066ab6cd81b4ec
Christopher Selden <cbpselden@gmail.com>
2015-03-20 15:31:37 -0400

    cleaned up questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,2 @@
 class QuestionsController < ApplicationController
-
-  def index
-    @questions = Question.all
-  end
-
-  def show
-    @quesion = Question.find(params[:id])
-  end
-
-  def new
-    @question = Question.new
-  end
-
 end

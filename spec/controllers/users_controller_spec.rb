b6a5e1aab94d6d0d17a9e51d3fafee523a8102ad
bruce wayne <no1_ldc@hotmail.com>
2015-03-20 14:59:19 -0400

    created user_controller.rb

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+describe UsersController do
+  let(:user) {User.create(username: 'Dongchul',email: 'ldc@ldc.com' password: '1234')}
+
+  describe 'GET Users#new' do
+    it 'should render login page' do
+      new_user_path
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'users#create' do
+    it 'should redirect after post' do
+      user :create, {username: 'Chris', email: 'chris@chris.com', password: '12345'}
+      expect(response.status).to be_redirect
+    end
+  end
+
+  describe 'users#show' do
+    it 'should render the show page' do
+      get "/users/#{user.id}"
+      expect(last_response).to be_ok
+    end
+  end
+
+end

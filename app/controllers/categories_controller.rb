26e47919efe4ee25c647bfc26dc054d0e52ac943
gabalicious <gabalicious113@gmail.com>
2015-03-20 08:34:13 -0400

    Added Index and Create Methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.where("question_id = '#{params[:question_id]}'")
+	end
+
+	def new
+		
+	end
+
+	def create
+		category = Category.create(:tag_name => params[:tag_name], :question_id => params[:question_id])
+	end
+
 end

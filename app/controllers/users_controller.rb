8872f03311e0c73a71f9d107196d3d3e865fcf7f
bruce wayne <no1_ldc@hotmail.com>
2015-03-20 18:56:43 -0400

    Fix user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password )
+    params.require(:user).permit(:name, :email, :password, :password_digest )
   end
 end

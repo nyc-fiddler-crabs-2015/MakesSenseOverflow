c812e3a40774e7c73bd3601d96f7048fb3564c9e
Christopher Selden <cbpselden@gmail.com>
2015-03-20 12:10:05 -0400

    added bare answer controller spec

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,22 @@
+describe ContactsController do
+  describe "GET #index" do
+    it 'populates a page with all of the questions'
+
+    it "renders the :index view"
+
+  end
+
+  describe "GET #show" do
+    it "assigns the requested answer to @answer"
+
+    it "renders the :show template"
+
+  end
+
+  describe "GET #new" do
+    it "assigns a new Answer to @answer"
+
+    it "renders the :new template"
+
+  end
+

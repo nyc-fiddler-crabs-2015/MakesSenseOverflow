e30060b4163fe5f5f0c1df560bb892a05870c29a
bruce wayne <no1_ldc@hotmail.com>
2015-03-20 17:05:32 -0400

    Added view/user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      redirect_to "new"
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password )
+  end
 end

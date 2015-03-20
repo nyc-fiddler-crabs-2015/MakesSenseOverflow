54538859dde5a417f3321c708d26774da25581db
Christopher Selden & Justin Gaba & Dongchul Lee <pair+chrisseldo.gabalicious.i-love-ruby@devbootcamp.com>
2015-03-20 08:20:52 -0400

    added 'new', 'create' and 'destroy' controllers for answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,29 @@
 class AnswersController < ApplicationController
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    answer = Answer.new(answer_params)
+    if answer.save
+      redirect_to :back
+    else
+      redirect_to new_answer_path
+  end
+
+  def destroy
+    Answer.find(params[:id]).destroy
+    redirect_to :back
+  end
+
+
+private
+
+  def answer_params
+    params.require(:answer).permit(:contnet, :vote_count,
+                                   :question_id, :user_id)
+  end
+
 end
+

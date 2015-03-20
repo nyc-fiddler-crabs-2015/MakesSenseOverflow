4c4e0b03de8d2f20022a600ef5ee5028f64c29f2
bruce wayne <no1_ldc@hotmail.com>
2015-03-20 15:18:44 -0400

    create questin controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,12 @@
 class QuestionsController < ApplicationController
 
+  before_filter :ensure_current_user
+
+  skip_before_filter :ensure_current_user, :only => [:index, :show]
+
   def index
     @questions = Question.all
+    @answers = @question.answers
   end
 
   def show
@@ -10,6 +15,41 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      redirect_to :back
+    end
+  end
+
+
+  def edit
+    @question = Question.find_by(params[:id])
+  end
+
+  def update
+    @question = Question.find_by(params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to question_path(@question)
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to :back
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description, :user_id)
   end
 
 end
